88b0b175152be70ca69571dad302da20377d90f1
Dawson Richey <dawsonrichey@gmail.com>
2018-06-20 23:17:40 -0400

    Added Context  - DbContect DbSet the entity properties for food

---
+++ FoodAppModule/Context.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FoodAppModule.Modules;
+
+namespace FoodAppModule
+{
+	public class Context : DbContext
+	{
+		public DbSet<Food> Foods { get; set; }
+	}
+	
+	}
+

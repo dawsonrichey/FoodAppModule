68fc08b8809e3cfef8f5786262710dd5bb8222d9
Dawson Richey <dawsonrichey@gmail.com>
2018-06-20 22:10:30 -0400

    Project Initialized - Added Modules folder to hold all entites  - Created public Food class and passed entites

---
+++ FoodAppModule/Modules/Food.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FoodAppModule.Modules
+{
+	public class Food
+	{
+		/// 4 id type options
+		/// Id, ID, FoodId, FoodID
+		/// 
+		public int FoodID { get; set; }
+		public string FoodName { get; set; }
+		public int FoodQuantity { get; set; }
+		public double FoodCost { get; set; }
+		public DateTime PublishedOn { get; set; }
+	}
+}
